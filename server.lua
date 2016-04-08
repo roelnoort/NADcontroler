93aa5b1713b37a86a5387795028dd1100d7d4aa5
Roel Noort <roel.noort@gmail.com>
2016-04-08 21:47:21 +0200

    POST requests now work and send proper reply. Documentation for RS232 protocol added.

--- server.lua
+++ server.lua
@@ -9,16 +9,27 @@ srv=net.createServer(net.TCP)
         -- GET /favicon.ico HTTP/1.1
 
         restmethod = string.sub(payload,1, payload:find(" ")-1)
-        print("Method")
+        print("Method ["..restmethod.."]")
+
         if restmethod == "GET" then
             print("serve our simple website")
+            conn:send("HTTP/1.1 200 OK\n\n")
+            conn:send("<!DOCTYPE HTML>\n")
+            conn:send("<html><body>\n")
             conn:send("<h1>NAD T758 controler</h1>")
             conn:send("<p>This is the RS232 controler for the NAD T758.</p>")
             conn:send("<p>Use HTTP POST via </p>")
             conn:send("<p>http://&lt;hostname&gt;:&lt;port&gt;/switchnad/&lt;source number&gt;</p>")
             conn:send("<p>http://&lt;hostname&gt;:&lt;port&gt;/poweroff</p>")
+            conn:send("</body></html>\n")
         elseif restmethod == "POST" then
-            print("control the NAD")
+            print("control the NAD ["..payload:sub(6, 16).."]")
+            if payload:sub(6, 16) == "/switchnad/" then
+                print("switch to another input")
+            elseif payload:sub(6, 15) == "/poweroff/" then
+                print("power off the amp")
+            end
+            conn:send("HTTP/1.1 200 OK\n\n")
         end
 
         --conn:close()
